2665da13bd9a08ec6fc39ea506a789bd2a108d28
Jesse Rusak <me@jesserusak.com>
2020-02-08 14:07:51 -0500

    Api changes

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 
-function main(name, payload)
+function main(sender, name, payload)
   if name == "say" then 
-    for _, object in ipairs(orisa.get_children(orisa.id)) do
-      orisa.send(object, "tell", {message = string.format("%s: %s", orisa.name(orisa.sender), payload)})
+    for _, object in ipairs(orisa.get_children(orisa.self)) do
+      orisa.send(object, "tell", {message = string.format("%s: %s", orisa.get_name(sender), payload)})
     end
-  elseif name == "tell" and orisa.kind == "system/user" then
+  elseif name == "tell" and orisa.get_kind(orisa.self) == "system/user" then
     orisa.tell(payload.message)
   else
     print("unknown message", name)
