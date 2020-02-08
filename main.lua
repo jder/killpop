ac7bdc55c5b9193ac6f3aded5756e2f7e8b1fc1f
Jesse Rusak <me@jesserusak.com>
2020-02-08 14:34:39 -0500

    state

--- main.lua
+++ main.lua
@@ -6,6 +6,11 @@ function main(sender, name, payload)
     end
   elseif name == "tell" and orisa.get_kind(orisa.self) == "system/user" then
     orisa.tell(payload.message)
+    last = orisa.get_state(orisa.self, "last")      
+    if last then
+      orisa.tell("Last thing said was " .. last)
+    end
+    orisa.set_state(orisa.self, "last", payload.message)
   else
     print("unknown message", name)
   end
