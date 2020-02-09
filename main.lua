6fa00cd07e5abc83e4a2ac32e3a24f44a8e1e958
Jesse Rusak <me@jesserusak.com>
2020-02-08 21:12:48 -0500

    allow eval

--- main.lua
+++ main.lua
@@ -1,8 +1,28 @@
 
 function main(sender, name, payload)
   if name == "say" then 
-    for _, object in ipairs(orisa.get_children(orisa.self)) do
-      orisa.send(object, "tell", {message = string.format("%s: %s", orisa.get_name(sender), payload)})
+    if orisa.get_kind(orisa.self) == "system/user" then
+      -- Backtick means "evaluate this"
+      first, last, cmd = string.find(payload, '^`(.*)')
+      if first then
+        chunk, err = load("return (" .. cmd .. ")", "command", "t")
+        if not chunk then
+          orisa.tell("Compile Error: " .. err)
+        else
+          success, result = pcall(chunk)
+          if success then
+            orisa.tell("Result: " .. tostring(result))
+          else
+            orisa.tell("Runtime Error: " .. tostring(result))
+          end
+        end
+      else 
+        orisa.send(orisa.get_parent(orisa.self), "say", payload)
+      end
+    elseif orisa.get_kind(orisa.self) == "system/room" then
+      for _, object in ipairs(orisa.get_children(orisa.self)) do
+        orisa.send(object, "tell", {message = string.format("%s: %s", orisa.get_name(sender), payload)})
+      end
     end
   elseif name == "tell" and orisa.get_kind(orisa.self) == "system/user" then
     orisa.tell(payload.message)
