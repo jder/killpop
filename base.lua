92da83476e5343c7d7bfb8e8f88cdf88d2a16e04
Jesse Rusak <me@jesserusak.com>
2020-02-09 18:06:08 -0500

    simple looking an inspecting

--- base.lua
+++ base.lua
@@ -1,5 +1,48 @@
 local base = {}
 
-base["from_base"] = 5
+-- patterns should be a map from lua patterns -> functions to call with the captures
+function base.parse(text, patterns)
+  for pat, handler in pairs(patterns) do
+    local captures = {string.match(text, pat)}
+    if captures[1] ~= nil then
+      handler(table.unpack(captures))
+      return true
+    end
+  end
+  return false
+end
+
+function base.get_name(object)
+  local username = orisa.get_username(object)
+  if username ~= nil then 
+    return username
+  end
+
+  local custom_name = orisa.get_attr(object, "name")
+  if custom_name ~= nil then
+    return custom_name
+  end
+
+  return "object"
+end
+
+function base.find(query, parent)
+  if string.match(query, "#%d+") then return query end
+
+  if parent == nil then
+    parent = orisa.get_parent(orisa.self)
+  end
+
+  if parent == nil then return nil end 
+  for _, child in ipairs(orisa.get_children(parent)) do
+    if base.get_name(child) == query then
+      return child
+    end
+  end
+
+  -- TODO: adjectives etc
+
+  return nil
+end
 
 return base
--- main.lua
+++ main.lua
@@ -10,24 +10,78 @@ function main(kind, sender, name, payload)
   end
 end
 
-function handle_system_user(kind, sender, name, payload)
-  if name == "say" then 
-    -- Backtick means "evaluate this"
-    local first, last, cmd = string.find(payload, '^`(.*)')
-    if first then
-      local chunk, err = load("return (" .. cmd .. ")", "command", "t")
-      if not chunk then
-        orisa.send_user_tell("Compile Error: " .. err)
-      else
-        local success, result = pcall(chunk)
-        if success then
-          orisa.send_user_tell("Result: " .. tostring(result))
-        else
-          orisa.send_user_tell("Runtime Error: " .. tostring(result))
-        end
+function run_eval(cmd) 
+  local chunk, err = load("return (" .. cmd .. ")", "command", "t")
+  if not chunk then
+    orisa.send_user_tell("Compile Error: " .. err)
+  else
+    local success, result = pcall(chunk)
+    if success then
+      orisa.send_user_tell("Result: " .. tostring(result))
+    else
+      orisa.send_user_tell("Runtime Error: " .. tostring(result))
+    end
+  end
+end
+
+function run_look()
+  local room = orisa.get_parent(orisa.self)
+  if not room then
+    orisa.send_user_tell("You aren't anywhere.")
+  else 
+    orisa.send_user_tell(base.get_name(room))
+    local description = orisa.get_attr(room, "description")
+    if description then
+      orisa.send_user_tell(description)
+    else
+      orisa.send_user_tell("It's unremarkable.")
+    end
+
+    local children = orisa.get_children(room)
+    local contents = "Present here: "
+    for i, child in ipairs(children) do
+      if i ~= 1 then
+        contents = contents .. ", "
       end
-    else 
-      orisa.send(orisa.get_parent(orisa.self), "say", payload)
+      contents = contents .. base.get_name(child) .. " (" .. child .. ")"
+    end
+    orisa.send_user_tell(contents)
+  end
+end
+
+function run_inspect(query)
+  local target = base.find(query)
+  if target == nil then 
+    orisa.send_user_tell("I don't see " .. query)
+    return
+  end
+
+  local description = orisa.get_attr(target, "description")
+  if description == nil then
+    orisa.send_user_tell(base.get_name(target) .. " is uninteresting.")
+  else
+    orisa.send_user_tell(base.get_name(target) .. ": " .. description)
+  end
+end
+
+
+function handle_system_user(kind, sender, name, payload)
+  if name == "say" then
+    local patterns = {
+      -- Backtick means "evaluate this"
+      ["^`(.*)"] = run_eval,
+      ["^/look$"] = run_look,
+      ["^/l$"] = run_look,
+      ["^/inspect *(.*)"] = run_inspect,
+      ["^/x *(.*)"] = run_inspect
+    }
+    if not base.parse(payload, patterns) then
+      local unknown = string.match(payload, "^/(%g*)")
+      if unknown ~= nil then
+        orisa.send_user_tell("Unknown command " .. unknown)
+      else 
+        orisa.send(orisa.get_parent(orisa.self), "say", payload)
+      end
     end
   elseif name == "tell" then
     orisa.send_user_tell(payload.message)
@@ -48,8 +102,11 @@ function handle_system_user(kind, sender, name, payload)
       orisa.send_user_backlog(history)
     end
     orisa.send_user_tell("Welcome Back! New features include lua errors appearing in console and backtick (`) meaning eval!")
+    orisa.send(orisa.get_parent(orisa.self), "say", "(wakes up)")
   elseif name == "save_file" then
     orisa.send_save_custom_space_content(payload.name, payload.content)
+  elseif name == "disconnected" then
+    orisa.send(orisa.get_parent(orisa.self), "say", "(goes to sleep)")
   else
     print("unknown message", name)
   end
@@ -58,9 +115,9 @@ end
 function handle_system_room(kind, sender, name, payload)
   if name == "say" then 
     for _, object in ipairs(orisa.get_children(orisa.self)) do
-      orisa.send(object, "tell", {message = string.format("%s: %s", orisa.get_name(sender), payload)})
+      orisa.send(object, "tell", {message = string.format("%s: %s", orisa.get_username(sender), payload)})
     end
   else
     print("unknown message", name)
-  end  
+  end
 end
