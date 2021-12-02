2cbba3eb872b46ebe319baeb897abae3da04ec50
Jesse Rusak <me@jesserusak.com>
2021-12-01 21:15:24 -0500

    typos

--- util.lua
+++ util.lua
@@ -155,7 +155,7 @@ end
 
 --- Does the text given describe this object? (Assuming both are lowercase & space-separated.)
 function util.object_matches(object, text)
-  local strip_articles = string.match(query, "the|a (.+))")
+  local strip_articles = string.match(text, "the|a (.+))")
   if strip_articles then text = strip_articles end
 
   if util.get_name(object) == text then
